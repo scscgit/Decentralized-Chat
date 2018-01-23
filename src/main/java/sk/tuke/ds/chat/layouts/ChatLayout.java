6aa9e22f0cf942a6e1de53fdbb326dcffb90ecaa
scsc <scscgit@users.noreply.github.com>
2018-01-23 04:31:10 +0100

    Implemented a working GUI skeleton for sending messages.
    Devised a custom hierarchical naming lookup system within cloned tabs.

--- src/main/java/sk/tuke/ds/chat/layouts/ChatLayout.java
+++ src/main/java/sk/tuke/ds/chat/layouts/ChatLayout.java
@@ -1,13 +1,15 @@
 package sk.tuke.ds.chat.layouts;
 
+import javafx.scene.input.KeyCode;
 import sk.tuke.ds.chat.util.Util;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class ChatLayout {
-    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane;
     private JPanel mainPanel;
     private JTextField nodeIpTextField;
     private JTextField nodePortTextField;
@@ -20,24 +22,73 @@ public class ChatLayout {
     private JPanel contentPanel;
     private JTextPane messagesTextPane;
     private JList usersList;
-    private JTextArea textArea1;
+    private JTextArea messageTextArea;
     private JButton sendButton;
+    private JPanel templateTab;
+    private JPanel userSettingsPanel;
+    private JPanel statusPanel;
 
     public ChatLayout() {
-        connectButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Connect to IP
-                tabbedPane1.addTab(
-                        nodeIpTextField.getText() + ":" + nodePortTextField.getText(),
-                        Util.copySerializable(tabbedPane1.getComponentAt(1))
-                );
+        // Manually set names for the objects that are gonna be looked up (including their cloned versions)
+        messageTextArea.setName("messageTextArea");
+        messagesTextPane.setName("messagesTextPane");
+
+        connectButton.addActionListener(e -> {
+            // Connect to IP
+            tabbedPane.addTab(
+                    nodeIpTextField.getText() + ":" + nodePortTextField.getText(),
+                    generateListeners(Util.copySerializable(templateTab))
+            );
+        });
+        hostButton.addActionListener(e -> {
+            // Host new server
+        });
+
+        // Example template tab is also used to test the listener generation on (Optional for testing)
+        generateListeners(templateTab);
+
+        // Hide the template though
+        this.tabbedPane.remove(templateTab);
+    }
+
+    private <T extends JPanel> T generateListeners(T tabPanel) {
+        Util.findComponentIn(tabPanel, "messageTextArea").addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+                JTextArea tabMessageTextArea = Util.findComponentIn(tabPanel, "messageTextArea");
+                String trimmedMessage = tabMessageTextArea.getText().trim();
+                if (trimmedMessage.equals("\n") || trimmedMessage.equals("")) {
+                    return;
+                }
+                if (e.getKeyCode() == KeyCode.ENTER.impl_getCode()) {
+                    // Send message via ENTER
+                    sendMessage(tabMessageTextArea);
+                }
             }
         });
-        hostButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                // Host new server
+        return tabPanel;
+    }
+
+    private void sendMessage(JTextArea textArea) {
+        // Trim newline
+        String message = textArea.getText().substring(0, textArea.getText().length() - 2);
+        textArea.setText("");
+
+        // TODO send via network
+
+        boolean found = false;
+        for (Component component : textArea.getParent().getComponents()) {
+            if ("messagesTextPane".equals(component.getName())) {
+                JTextPane messages = (JTextPane) component;
+                messages.setText(messages.getText() + "\n" + "MSG > " + message);
+                found = true;
+                break;
             }
-        });
+        }
+        if (!found) {
+            throw new RuntimeException("messagesTextPane not found in parent" + textArea.getParent().getName());
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/sk/tuke/ds/chat/util/Util.java
+++ src/main/java/sk/tuke/ds/chat/util/Util.java
@@ -1,5 +1,6 @@
 package sk.tuke.ds.chat.util;
 
+import java.awt.*;
 import java.io.*;
 
 public class Util {
@@ -21,5 +22,31 @@ public class Util {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
+    }
+
+    public static <T extends Component> T findComponentIn(Container inContainer, String name) {
+        if (name == null || "".equals(name)) {
+            throw new RuntimeException("No name for finding components provided");
+        }
+        T componentInRecursively = findComponentInRecursively(inContainer, name);
+        if (componentInRecursively != null) {
+            return componentInRecursively;
+        }
+        throw new RuntimeException("Didn't find component named " + name + " in " + inContainer.getName());
+    }
+
+    private static <T extends Component> T findComponentInRecursively(Container inContainer, String name) {
+        for (Component component : inContainer.getComponents()) {
+            if (name.equals(component.getName())) {
+                return (T) component;
+            }
+            if (component instanceof Container) {
+                T componentInRecursively = findComponentInRecursively(((Container) component), name);
+                if (componentInRecursively != null) {
+                    return componentInRecursively;
+                }
+            }
+        }
+        return null;
     }
 }
