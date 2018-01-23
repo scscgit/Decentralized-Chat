234c9e6513cda9a73af74e5e83774b9857aa6ed4
scsc <scscgit@users.noreply.github.com>
2018-01-23 22:58:13 +0100

    Created chat tab representation class and mocked message sending.

--- src/main/java/sk/tuke/ds/chat/layouts/ChatLayout.java
+++ src/main/java/sk/tuke/ds/chat/layouts/ChatLayout.java
@@ -4,9 +4,9 @@ import javafx.scene.input.KeyCode;
 import sk.tuke.ds.chat.util.Util;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Date;
 
 public class ChatLayout {
     private JTabbedPane tabbedPane;
@@ -30,71 +30,76 @@ public class ChatLayout {
 
     public ChatLayout() {
         // Manually set names for the objects that are gonna be looked up (including their cloned versions)
-        messageTextArea.setName("messageTextArea");
-        messagesTextPane.setName("messagesTextPane");
+        this.messageTextArea.setName("messageTextArea");
+        this.messagesTextPane.setName("messagesTextPane");
+        this.usernameTextField.setName("usernameTextField");
 
-        connectButton.addActionListener(e -> {
+        // Configuration listeners
+        this.connectButton.addActionListener(e -> {
             // Connect to IP
-            tabbedPane.addTab(
-                    nodeIpTextField.getText() + ":" + nodePortTextField.getText(),
-                    generateListeners(Util.copySerializable(templateTab))
+            JPanel tabPanel = generateListeners(Util.copySerializable(this.templateTab));
+            this.tabbedPane.addTab(
+                    this.nodeIpTextField.getText() + ":" + this.nodePortTextField.getText(),
+                    tabPanel
             );
+            this.tabbedPane.setSelectedComponent(tabPanel);
+            ChatTab.lookup(tabPanel).generateUsername();
         });
-        hostButton.addActionListener(e -> {
+        this.hostButton.addActionListener(e -> {
             // Host new server
         });
 
         // Example template tab is also used to test the listener generation on (Optional for testing)
-        generateListeners(templateTab);
+        generateListeners(this.templateTab);
 
         // Hide the template though
-        this.tabbedPane.remove(templateTab);
+        this.tabbedPane.remove(this.templateTab);
     }
 
     private <T extends JPanel> T generateListeners(T tabPanel) {
+        // Tab configuration
+        this.renameUserButton.addActionListener(e -> {
+            ChatTab.lookup(tabPanel).setUsername(
+                    Util.<JTextField>findComponentIn(tabPanel, "usernameTextField").getText()
+            );
+        });
+        this.disconnectButton.addActionListener(e -> {
+            this.tabbedPane.remove(tabPanel);
+            ChatTab.lookup(tabPanel).onDisconnect();
+        });
+
+        // Sending messages
         Util.findComponentIn(tabPanel, "messageTextArea").addKeyListener(new KeyAdapter() {
             @Override
             public void keyReleased(KeyEvent e) {
                 super.keyReleased(e);
-                JTextArea tabMessageTextArea = Util.findComponentIn(tabPanel, "messageTextArea");
-                String trimmedMessage = tabMessageTextArea.getText().trim();
-                if (trimmedMessage.equals("\n") || trimmedMessage.equals("")) {
-                    return;
-                }
                 if (e.getKeyCode() == KeyCode.ENTER.impl_getCode()) {
                     // Send message via ENTER
-                    sendMessage(tabMessageTextArea);
+                    sendMessage(tabPanel);
                 }
             }
         });
         return tabPanel;
     }
 
-    private void sendMessage(JTextArea textArea) {
+    private void sendMessage(JPanel tabPanel) {
+        JTextArea textArea = Util.findComponentIn(tabPanel, "messageTextArea");
+
         // Trim newline
-        String message = textArea.getText().substring(0, textArea.getText().length() - 2);
+        String[] messages = textArea.getText().split("\n");
         textArea.setText("");
 
         // TODO send via network
 
-        boolean found = false;
-        for (Component component : textArea.getParent().getComponents()) {
-            if ("messagesTextPane".equals(component.getName())) {
-                JTextPane messages = (JTextPane) component;
-                messages.setText(messages.getText() + "\n" + "MSG > " + message);
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            throw new RuntimeException("messagesTextPane not found in parent" + textArea.getParent().getName());
-        }
+        // Temporary
+        ChatTab lookup = ChatTab.lookup(tabPanel);
+        lookup.addMessage(lookup.getUsername(), messages, new Date());
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("sk.tuke.ds.chat.ChatLayout");
+        JFrame frame = new JFrame("Decentralized Chat by Steve (ɔ)");
         frame.setContentPane(new ChatLayout().mainPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
---
+++ src/main/java/sk/tuke/ds/chat/layouts/ChatTab.java
@@ -0,0 +1,67 @@
+package sk.tuke.ds.chat.layouts;
+
+import sk.tuke.ds.chat.util.Util;
+
+import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class ChatTab {
+
+    private static final Map<JPanel, ChatTab> chatTabMap = new HashMap<>();
+
+    private JPanel tabPanel;
+    private String username;
+
+    private ChatTab(JPanel tabPanel) {
+        if (tabPanel == null) {
+            throw new RuntimeException("Null value of tab panel provided");
+        }
+        this.tabPanel = tabPanel;
+    }
+
+    public static synchronized ChatTab lookup(JPanel tabPanel) {
+        if (!ChatTab.chatTabMap.containsKey(tabPanel)) {
+            ChatTab.chatTabMap.put(tabPanel, new ChatTab(tabPanel));
+        }
+        return ChatTab.chatTabMap.get(tabPanel);
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+        Util.<JTextField>findComponentIn(tabPanel, "usernameTextField").setText(username);
+    }
+
+    public void generateUsername() {
+        setUsername("guest" + (new Random().nextInt(900) + 100));
+    }
+
+    public void onDisconnect() {
+    }
+
+    public void addMessage(String username, String[] messages, Date date) {
+        JTextPane messagesTextPane = Util.findComponentIn(this.tabPanel, "messagesTextPane");
+        for (String message : messages) {
+            message = message.trim();
+            if (!"".equals(message)) {
+                messagesTextPane.setText(
+                        messagesTextPane.getText()
+                                + "\n"
+                                + (
+                                new SimpleDateFormat("dd.M. hh:mm:ss").format(date)
+                                        + " ["
+                                        + username
+                                        + "] > "
+                                        + message)
+                );
+            }
+        }
+    }
+}
